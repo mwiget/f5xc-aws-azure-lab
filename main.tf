ba827455e85cbba0568abe135a5f9a52e08b0a2d
Marcel Wiget <mwiget@gmail.com>
2022-07-27 15:16:15 +0200

    using env var TF_VAR_ for sensitive data

--- main.tf
+++ main.tf
@@ -20,13 +20,13 @@ module "azure_site_1a" {
   f5xc_azure_default_ce_os_version    = true
   f5xc_azure_no_worker_nodes          = true
   f5xc_azure_total_worker_nodes       = 0
-  public_ssh_key                      = "ssh-rsa ......"
+  public_ssh_key                      = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDr1zH+NmWzf/+qtCTqC/+QAHoWIoq3k3YjH/IsjYdHXZ0mQonsMlrL+owArvLtvi3gXxqPGlO/aWt53v8KAY+RV7IOSbqfFY56k0GTmvPJisSsBkAedruu05hqlFMS/2mkNFL/BsWNzL617LtuFQpN6ud57QSrQruQQtIKTuWUe+XjqkSNiAkvD4zc4tip9ovULhC9QY/IVmhguVDJ0FuQWCDd4l7IM+KjlTXGplN5Y9bIVuU+nnSHnUEkRFxuGX1pvOHB1L31INlD9CVJHDA6bBJyIQgv0WcqoA2/3/8eRqN/pXOe+clglJGRT6bb/+5Sfy6JZoA0OlsyW66VfGR3 mwiget@xeon"
 } 
 
 module "site_status_check" {
   source            = "./modules/f5xc/status/site"
   f5xc_api_url      = "https://playground.staging.volterra.us/api"
-  f5xc_api_token    = "............................"
+  f5xc_api_token    = var.f5xc_api_token
   f5xc_namespace    = "system"
   f5xc_tenant       = "playground-wtppvaog"
   f5xc_site_name    = "mw-azure-site-1a"
