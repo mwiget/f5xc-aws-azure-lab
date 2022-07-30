4d521dbdeedeb85306e9cec8c3a16d912cd3cf7e
Marcel Wiget <mwiget@gmail.com>
2022-07-30 22:11:26 +0200

    renamed site, added 3b

--- aws_site3.tf
+++ aws_site3.tf
@@ -1,3 +1,81 @@
+module "aws_site_3a" {
+  source                          = "./modules/f5xc/site/aws/vpc"
+  f5xc_api_p12_file              = "cert/playground.staging.api-creds.p12"
+  f5xc_api_ca_cert               = "cert/public_server_ca.crt"
+  f5xc_api_url                   = "https://playground.staging.volterra.us/api"
+  f5xc_namespace                 = "system"
+  f5xc_tenant                    = "playground-wtppvaog"
+  f5xc_aws_region                 = "us-west-2"
+  f5xc_aws_cred                   = "mw-aws-f5"
+  f5xc_aws_vpc_site_name          = "mw-aws-site-3a"
+  f5xc_aws_vpc_name_tag           = ""
+  f5xc_aws_vpc_id                 = module.aws_vpc_3.aws_vpc_id
+  f5xc_aws_vpc_total_worker_nodes = 0
+  f5xc_aws_ce_gw_type             = "single_nic"
+  aws_owner_tag                   = var.owner_tag
+  f5xc_aws_vpc_az_nodes           = {
+    node0 : { 
+      f5xc_aws_vpc_id           = module.aws_vpc_3.aws_vpc_id,
+      f5xc_aws_vpc_local_subnet = module.aws_subnet_3.aws_subnet_id[0], 
+      f5xc_aws_vpc_az_name      = "us-west-2a" }
+  }
+  f5xc_aws_default_ce_os_version       = true
+  f5xc_aws_default_ce_sw_version       = true
+  f5xc_aws_vpc_no_worker_nodes         = false
+  f5xc_aws_vpc_use_http_https_port     = true
+  f5xc_aws_vpc_use_http_https_port_sli = true
+  public_ssh_key                       = "${file(var.ssh_public_key_file)}"
+}
+
+module "aws_site_3b" {
+  source                          = "./modules/f5xc/site/aws/vpc"
+  f5xc_api_p12_file              = "cert/playground.staging.api-creds.p12"
+  f5xc_api_ca_cert               = "cert/public_server_ca.crt"
+  f5xc_api_url                   = "https://playground.staging.volterra.us/api"
+  f5xc_namespace                 = "system"
+  f5xc_tenant                    = "playground-wtppvaog"
+  f5xc_aws_region                 = "us-west-2"
+  f5xc_aws_cred                   = "mw-aws-f5"
+  f5xc_aws_vpc_site_name          = "mw-aws-site-3b"
+  f5xc_aws_vpc_name_tag           = ""
+  f5xc_aws_vpc_id                 = module.aws_vpc_3.aws_vpc_id
+  f5xc_aws_vpc_total_worker_nodes = 0
+  f5xc_aws_ce_gw_type             = "single_nic"
+  aws_owner_tag                   = var.owner_tag
+  f5xc_aws_vpc_az_nodes           = {
+    node0 : { 
+      f5xc_aws_vpc_id           = module.aws_vpc_3.aws_vpc_id,
+      f5xc_aws_vpc_local_subnet = module.aws_subnet_3.aws_subnet_id[1], 
+      f5xc_aws_vpc_az_name      = "us-west-2a" }
+  }
+  f5xc_aws_default_ce_os_version       = true
+  f5xc_aws_default_ce_sw_version       = true
+  f5xc_aws_vpc_no_worker_nodes         = false
+  f5xc_aws_vpc_use_http_https_port     = true
+  f5xc_aws_vpc_use_http_https_port_sli = true
+  public_ssh_key                       = "${file(var.ssh_public_key_file)}"
+}
+
+module "site_status_check_3a" {
+  source            = "./modules/f5xc/status/site"
+  f5xc_api_url      = "https://playground.staging.volterra.us/api"
+  f5xc_api_token    = var.f5xc_api_token
+  f5xc_namespace    = "system"
+  f5xc_tenant       = "playground-wtppvaog"
+  f5xc_site_name    = "mw-aws-site-3a"
+  depends_on        = [module.aws_site_3a]
+}
+
+module "site_status_check_3b" {
+  source            = "./modules/f5xc/status/site"
+  f5xc_api_url      = "https://playground.staging.volterra.us/api"
+  f5xc_api_token    = var.f5xc_api_token
+  f5xc_namespace    = "system"
+  f5xc_tenant       = "playground-wtppvaog"
+  f5xc_site_name    = "mw-aws-site-3b"
+  depends_on        = [module.aws_site_3b]
+}
+
 module "aws_workload_3a" {
   source                = "./modules/aws/ec2ubuntu"
   aws_ec2_instance_name = "mw-aws-site-3a"
