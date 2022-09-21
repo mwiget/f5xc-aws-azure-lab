86f959b491323f13a831b7f4f87f448864148bea
Marcel Wiget <mwiget@gmail.com>
2022-09-21 14:22:24 +0200

    use generic name, will be deployed across clouds

--- workload_custom_data.sh
+++ workload_custom_data.sh
@@ -5,8 +5,8 @@ systemctl enable snap.docker.dockerd
 systemctl start snap.docker.dockerd
 sleep 30
 docker run -d --name=tailscaled -v /var/lib:/var/lib -v /dev/net/tun:/dev/net/tun --network=host --privileged tailscale/tailscale tailscaled --state=/tmp/tailscaled.state
-docker run -d --net=host --restart=always \-e F5DEMO_APP=text \-e F5DEMO_NODENAME='Azure Environment' \-e F5DEMO_COLOR=ffd734 \
--e F5DEMO_NODENAME_SSL='Azure Environment (Backend App)' \
+docker run -d --net=host --restart=always \-e F5DEMO_APP=text \-e F5DEMO_NODENAME='Workload site Environment' \-e F5DEMO_COLOR=ffd734 \
+-e F5DEMO_NODENAME_SSL='Workload Environment (Backend App)' \
 -e F5DEMO_COLOR_SSL=a0bf37 \
 -e F5DEMO_BRAND=volterra \
 public.ecr.aws/y9n2y5q5/f5-demo-httpd:openshift
