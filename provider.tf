12d410fb60749ad0163d9d8330bd32025c083038
Marcel Wiget <mwiget@gmail.com>
2022-10-27 13:28:56 +0200

    version upgrade

--- provider.tf
+++ provider.tf
@@ -21,13 +21,18 @@ provider "aws" {
 }
 
 provider "aws" {
-  alias = "us_west_1"
-  region = "us-west-1"
+  alias = "us_east_1"
+  region = "us-east-1"
 }
 
 provider "aws" {
   alias = "us_west_2"
   region = "us-west-2"
+}
+
+provider "aws" {
+  alias = "us_east_2"
+  region = "us-east-2"
 }
 
 provider "google" {
